8c34679dfbb415a58fb52341c69d2800931203ad
Christian Sanders <christiansanders@ComputerMindss-Mac-mini.local>
2025-10-24 11:29:31 +0100

    Added a bunch of SEO tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,6 @@
 	<p class="mt-5 mb-3">Some banks offer a challenge called the "1 penny everyday challenge". What this challenge does is it takes 1 penny from your current account on day 1. Each day increases by 1 penny. So day 2 is 2p, day 3 is 3p taken etc and so forth all the way up to day 365.</p>
 	<p class="mb-3">The aim of this is to stop you spending money every month by taking alittle away from your account on a daily basis. The result? £667.95 will be waiting for you at the end of the challenge if you saved every day!</p>
 	<p class="mb-10">This calculator helps you to know how many pennies will be taken from your account in any given month.</p>
-
 	<h3 class="mb-5 border-b border-blue-500 pb-1 w-fit">Calculator</h3>
 	<Form />
 </div>
