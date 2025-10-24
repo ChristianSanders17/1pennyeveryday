9294523a8c361fd4d43fa93fb83b88ed41f4fdc7
Christian Sanders <christiansanders@ComputerMindss-Mac-mini.local>
2025-10-24 09:46:29 +0100

    Typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,6 @@
 <div class="mx-auto w-full">
 	<h1 class="mt-10 xl:mt-30 w-fit pb-4">The 1 Penny everyday challenge calculator</h1>
 	<span class="block border-t border-b-blue-500 text-blue-500"></span>
-	<p class="mb-10 xl:mb-15 mt-5">Some banks offer a challenge called the "1penny everyday challenge". What this challegne does is it takes 1 penny from your current account on day 1. Each day increases by 1 penny. So day 2 is 2p, day 3, is 3p taken etc you get the jist. This calculator helps you to know how many pennies will be taken from your account in any given month.</p>
+	<p class="mb-10 xl:mb-15 mt-5">Some banks offer a challenge called the "1 penny everyday challenge". What this challegne does is it takes 1 penny from your current account on day 1. Each day increases by 1 penny. So day 2 is 2p, day 3, is 3p taken etc you get the jist. This calculator helps you to know how many pennies will be taken from your account in any given month.</p>
 	<Form />
 </div>
